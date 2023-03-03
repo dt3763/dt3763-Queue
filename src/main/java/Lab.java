e23492a68ec8616510a7b215ef067f0506b7d9bb
dt3763 <dt3763@gmail.com>
2023-03-03 08:49:05 +0000

    queues

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,7 @@
 import java.util.Deque;
+
 import java.util.Queue;
+import java.util.ArrayDeque;
 
 /**
  * Queues are FIFO - first in, first out, like a checkout line. They are used in algorithms where it is needed to keep
@@ -36,7 +38,8 @@ public class Lab {
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        Deque<String> deque = new ArrayDeque<>();
+        return deque;
     }
 
     /**
@@ -45,7 +48,7 @@ public class Lab {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        return queue.size();
     }
 
     /**
@@ -55,8 +58,13 @@ public class Lab {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
+        queue.add(item);
 
+    
+   
+        
     }
+
 
     /**
      * Remove an item from a queue, and return the removed item.
@@ -64,9 +72,14 @@ public class Lab {
      * @param queue a Queue<String> object.
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
-    public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+    public static String removeFromStartOfQueue(Queue<String> queue){   
+        
+       
+
+         return queue.remove();
     }
+
+   
 
     /**
      * Return the next item from a queue which is due to be removed, but do not remove it. This is referred to as
@@ -75,6 +88,23 @@ public class Lab {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        return queue.peek();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
